ab0a7bdac261d99f6deb007c2c6fd941f7a2c357
R0muken <romanklymak@gmail.com>
2023-03-27 16:40:15 +0300

    Updated json_generator.py

--- json_generator.py
+++ json_generator.py
@@ -6,7 +6,7 @@ import os
 import datetime
 import argparse
 
-
+# Arguments
 parser = argparse.ArgumentParser(prog='Json_generator')
 
 parser.add_argument('-c', '--count')
@@ -19,8 +19,6 @@ parser.add_argument('-e', '--end')
 
 args = parser.parse_args()
 
-
-fake = Faker()
 
 # Write process id in file
 with open('pid.txt', 'w') as f:
@@ -45,6 +43,7 @@ table_data = ['{market_driver} monthly %',
 
 # Later version test data
 def generate_random_data(time):
+    fake = Faker()
     data = {
         "name": fake.name(),
         "age": random.randint(18, 65),
@@ -75,23 +74,6 @@ data = {
 }
 
 if __name__ == '__main__':
-    print(args)
-    # with open("data.json", "w") as f:
-    #     new_data = json.dumps(data, indent=4)
-    #     f.write(new_data)
-
-    # while True:
-    #     dt = datetime.datetime.now()
-    #     dt_str = dt.strftime("%Y-%m-%d %H:%M:%S")
-    #     with open("logs-example.json", "w") as f:
-    #         for i in range(1):
-    #             data['charts'][0]['timestamp'].append(dt_str)
-    #             data['charts'][0]['portfolio_value'].append(random.randint(2800, 3200))
-    #             data['charts'][1]['timestamp'].append(dt_str)
-    #             data['charts'][1]['price'].append(round(random.uniform(0.28, 0.30), 2))
-    #             # new_data = json.dumps(data, indent=4)
-    #             # f.write(new_data)
-    #     time.sleep(2)
 
     while True:
         dt = datetime.datetime.now()
@@ -105,20 +87,12 @@ if __name__ == '__main__':
                 data['charts'][1]['price'].append(round(random.uniform(0.28, 0.30), 2))
 
                 data["markers_table"][random.choice(table_data)] = [round(random.uniform(0.1, 0.8), 2)]
-                n = json.dumps(data, indent=4)
+                json_data = json.dumps(data, indent=4)
 
         with open("data.json", "w") as f:
-            f.write(n)
+            f.write(json_data)
 
+        print(json_data)
         time.sleep(2)
 
-#  For generate_random_data
-# while True:
-#     dt = datetime.datetime.now()
-#     dt_str = dt.strftime("%Y-%m-%d %H:%M:%S")
-#     with open("data.json", "a") as f:
-#         for i in range(1):
-#             data = generate_random_data(dt_str)
-#             json_data = json.dumps(data)
-#             f.write(json_data + "\n")
-#     time.sleep(2)
+
